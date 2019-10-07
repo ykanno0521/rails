70d8054a7f2b103abb203143447e7db2e7782f64
菅野裕介 <kanno.yusuke@MacBook-Pro.local>
2019-10-07 14:32:31 +0900

    [Finish]3-8

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_06_054245) do
+ActiveRecord::Schema.define(version: 2019_10_07_045211) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
 end
