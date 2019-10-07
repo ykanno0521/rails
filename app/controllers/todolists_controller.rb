6f3dcc02950a90a9328d1c488b261033c0698f00
ykanno0521 <kanno0521@gmail.com>
2019-10-07 03:46:53 +0000

    [Finish]3-7

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,7 +1,7 @@
 class TodolistsController < ApplicationController
   def new
   	  # Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
-      @list = List.new
+    @list = List.new
   end
 
   def create
@@ -10,8 +10,30 @@ class TodolistsController < ApplicationController
     # DBへ保存する
     list.save
     # トップ画面へリダイレクト
-        redirect_to '/top'
+    redirect_to todolist_path(list.id)
+    # 詳細画面へリダイレクト
   end
+
+  def index
+    @lists = List.all
+
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
+
+
 
   private
 
