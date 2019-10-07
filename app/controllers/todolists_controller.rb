39cbbaeabf1e3b351e07efbabcebb2041eec73d3
菅野裕介 <kanno.yusuke@MacBook-Pro.local>
2019-10-07 15:04:56 +0900

    [Finish]3-9

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -33,6 +33,12 @@ class TodolistsController < ApplicationController
     redirect_to todolist_path(list.id)
   end
 
+  def destroy
+    list = List.find(params[:id]) #データ(レコード)を1件取得
+    list.destroy #データ（レコード）を削除
+    redirect_to todolists_path #List一覧画面へリダイレクト
+    end
+
 
 
   private
